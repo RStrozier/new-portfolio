4161fffdee26a69dfe43608dd3e31312b8dfd74f
Strozier <strozierrashida@vineyardcolumbus.org>
2024-08-13 14:37:43 -0400

    hover button color change

--- src/sections/section-one/SectionOne.tsx
+++ src/sections/section-one/SectionOne.tsx
@@ -26,7 +26,7 @@ const SectionOne = () => {
             </div>
 
           <div className="center-container">
-          <div className="div">Rashida is a unique developer who has the desire to merge the world
+          <div className="text-center">Rashida is a unique developer who has the desire to merge the world 
             <br />
             online with the world we live in today through art and simple design.
           </div>
